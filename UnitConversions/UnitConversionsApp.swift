5360a03ce8c9051a3e4ef95d2dc7258a96bd830f
SeongYong_Song <songsy0612@gmail.com>
2025-09-10 20:22:14 +0900

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by SeongYongSong on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
