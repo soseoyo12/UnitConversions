2987fa53ae8986330f803e006ae29e17ab6f8a09
SeongYong_Song <songsy0612@gmail.com>
2025-09-10 20:49:47 +0900

    Picker

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var celsius = 20
+    @State private var fahrenheit = 70
+    @State private var kelvin = 19
+    
+    let unitArr = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    @State var selectedUnitFrom = "Celsius"
+    @State var selectedUnitTo = "Fahrenheit"
+    
+    @State var inputValue = 20
+    @State var outputValue = 100
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form{
+                Section("From"){
+                    Picker("from", selection: $selectedUnitFrom) {
+                        ForEach(unitArr, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("To"){
+                    Picker("to", selection: $selectedUnitTo) {
+                        ForEach(unitArr, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    TextField("Input", value: $inputValue, format: .currency(code: "USD"))
+                }
+                
+                
+                
+                
+            }
+            .navigationTitle("UnitConversions")
         }
-        .padding()
+        
+        
     }
 }
 
